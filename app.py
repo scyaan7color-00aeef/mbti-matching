e84bc3ccd54ea8c6fa98a6766366c3004a590bf5
scyaan7color-00aeef <s.cyaan7color@gmail.com>
2026-03-31 20:38:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def derive_mbti(e_score, s_score, t_score, j_score):
     return e + s + t + j
 
 # --- ページ設定 ---
-st.set_page_config(page_title="就活MBTIマッチング", layout="centered")
+set_page_config(page_title="就活MBTIマッチング", layout="centered")
 # カスタムCSS
 st.markdown("""
 <style>
